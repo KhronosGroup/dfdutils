f36db289643ec01ea21f8e4e7c6bf5985b139053
Rahul Sheth <rahul@snap.com>
2021-06-25 17:39:43 -0400

    Restore original vulkan and data format headers

--- third_party/KhronosGroup/khr_df.h
+++ third_party/KhronosGroup/khr_df.h
@@ -305,7 +305,6 @@ typedef enum _khr_df_model_e {
     /* PowerVR Texture Compression */
     KHR_DF_MODEL_PVRTC         = 164U,
     KHR_DF_MODEL_PVRTC2        = 165U,
-    KHR_DF_MODEL_UASTC         = 166U,
     /* Proprietary formats (ATITC, etc.) should follow */
     KHR_DF_MODEL_MAX = 0xFFU
 } khr_df_model_e;
@@ -491,23 +490,14 @@ typedef enum _khr_df_model_channels_e {
     /* MODEL_ASTC */
     KHR_DF_CHANNEL_ASTC_DATA  = 0U,
     /* MODEL_ETC1S */
-    KHR_DF_CHANNEL_ETC1S_RGB   = 0U,
-    KHR_DF_CHANNEL_ETC1S_RRR   = 3U,
-    KHR_DF_CHANNEL_ETC1S_GGG   = 4U,
-    KHR_DF_CHANNEL_ETC1S_AAA   = 15U,
+    KHR_DF_CHANNEL_ETC1S_DATA  = 0U,
+    KHR_DF_CHANNEL_ETC1S_COLOR = 0U,
     /* MODEL_PVRTC */
     KHR_DF_CHANNEL_PVRTC_DATA  = 0U,
     KHR_DF_CHANNEL_PVRTC_COLOR = 0U,
     /* MODEL_PVRTC2 */
     KHR_DF_CHANNEL_PVRTC2_DATA  = 0U,
     KHR_DF_CHANNEL_PVRTC2_COLOR = 0U,
-    /* MODEL UASTC */
-    KHR_DF_CHANNEL_UASTC_DATA  = 0U,
-    KHR_DF_CHANNEL_UASTC_RGB   = 0U,
-    KHR_DF_CHANNEL_UASTC_RGBA  = 3U,
-    KHR_DF_CHANNEL_UASTC_RRR   = 4U,
-    KHR_DF_CHANNEL_UASTC_RRRG  = 5U,
-    KHR_DF_CHANNEL_UASTC_RG    = 6U,
 
     /* Common channel names shared by multiple formats */
     KHR_DF_CHANNEL_COMMON_LUMA    =  0U,
@@ -567,7 +557,7 @@ typedef enum _khr_df_transfer_e {
     KHR_DF_TRANSFER_LINEAR      = 1U,
     /* Perceptually-linear transfer function of sRGH (~2.4) */
     KHR_DF_TRANSFER_SRGB        = 2U,
-    /* Perceptually-linear transfer function of ITU BT.601, BT.709 and BT.2020 (~1/.45) */
+    /* Perceptually-linear transfer function of ITU non-HDR specifications (~1/.45) */
     KHR_DF_TRANSFER_ITU         = 3U,
     /* SMTPE170M (digital NTSC) defines an alias for the ITU transfer function (~1/.45) */
     KHR_DF_TRANSFER_SMTPE170M   = 3U,
